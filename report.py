ab04f57e78fc82c41fe161a5644e197ff67b0f59
Muhummad Patel <muhummad.patel@gmail.com>
2016-05-14 12:39:50 +0200

    Add Mean Average Precision calculations before and after BRF.

--- report.py
+++ report.py
@@ -1,6 +1,9 @@
 import os
 import sys
 
+import query as query_tool
+import parameters
+
 def get_queries(collection):
     collection_filenames = os.listdir(collection)
     query_filenames = [name for name in collection_filenames if "query" in name]
@@ -33,10 +36,41 @@ def get_relevances(collection):
     return relevances
 
 
+def get_MAP(collection, queries, relevances):
+    query_count = 0
+    avg_precision_sum = 0.0
+    for query_idx, test_query in enumerate(queries):
+        # print("query:", test_query)
+        similarity, result, titles = query_tool.get_result(collection, test_query)
+
+        result_count = 0
+        precision_sum = 0.0
+        for i in range(len(result)):
+            result_count += 1
+            relevance_of_result = relevances[query_idx][int(result[i])-1] / 2.0
+            precision_sum += relevance_of_result / result_count
+
+            # print("{0:10.8f} {1:5} {2}".format(similarity[result[i]], result[i], titles[result[i]]), end=" ")
+            # print(relevance_of_result)
+        # print()
+
+        query_count += 1
+        avg_precision_sum += precision_sum / query_count
+
+    # print("~> Mean Average Precision:", avg_precision_sum / query_count)
+    return avg_precision_sum / query_count
+
+
 def main(collection):
     queries = get_queries(collection)
     relevances = get_relevances(collection)
 
+    parameters.BRF = False
+    map_before = get_MAP(collection, queries, relevances)
+    parameters.BRF = True
+    map_after = get_MAP(collection, queries, relevances)
+    print("MAP before BRF:", map_before)
+    print("MAP after BRF:", map_after)
 
 if __name__ == "__main__":
     main(sys.argv[1])
